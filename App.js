e740a648bff8b99c01434ca924885225decd3f51
chetu97 <chaitanya.sanoriya@gmail.com>
2021-03-10 00:42:31 -0500

    navigation in app.js

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+// import Students from "./components/Students";
+// import StudentDetail from "./components/StudentDetail";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+    <NavigationContainer>
+        <Stack.Navigator>
+            {/* <Stack.Screen name="Students" component={Students} />
+            <Stack.Screen name="StudentDetails" component={StudentDetail} /> */}
+        </Stack.Navigator>
+    </NavigationContainer>
+);
