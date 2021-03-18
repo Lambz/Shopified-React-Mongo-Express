6d35e64a2524ce25665ee76821fd1a9cef4412fe
Chetan Singh <chetan.singh.0602@gmail.com>
2021-03-18 17:10:04 -0400

    fixed app.js merge bugs

--- App.js
+++ App.js
@@ -1,28 +1,23 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-// import Students from "./components/Students";
-// import StudentDetail from "./components/StudentDetail";
+
 import SellerLogin from "./components/SellerLogin";
 import SellerSignUp from "./components/SellerSignUp";
 import SellerDashboard from "./components/SellerDashboard";
 import UserDashboard from "./components/UserDashboard";
-<<<<<<< HEAD
 import Browse from "./components/Browse";
 import ProductDetail from "./components/ProductDetail";
 import ProductsList from "./components/ProductsList";
 import ContactForBuy from "./components/ContactForBuy";
-=======
 import OldOrders from "./components/OldOrders";
 import OrderDetails from "./components/OrderDetails";
 import ChangePassword from "./components/ChangePassword";
-import ContactForBuy from "./components/ContactForBuy";
+
 import SignInScreen from "./LoginScreen";
 import SignUpScreen from "./SignUpScreen";
 import AddProuct from "./components/AddProduct";
-import ProductDetail from "./components/ProductDetail";
-import ProductsList from "./components/ProductsList";
->>>>>>> 76e083c87e80876c88bde44c53c26869f3c93c3a
+
 
 const Stack = createStackNavigator();
 
@@ -34,13 +29,6 @@ export default () => (
             {/* <Stack.Screen name="SellerLogin" component={SellerLogin} />
             <Stack.Screen name="SellerSignUp" component={SellerSignUp} />
             <Stack.Screen name="SellerDashboard" component={SellerDashboard} /> */}
-<<<<<<< HEAD
-            {/* <Stack.Screen name="UserDashboard" component={UserDashboard} /> */}
-            <Stack.Screen name="Browse" component={Browse} />
-            <Stack.Screen name="Products List" component={ProductsList} />
-            <Stack.Screen name="Product Detail" component={ProductDetail} />
-            <Stack.Screen name="Contact For Buy" component={ContactForBuy} />
-=======
 
             <Stack.Screen
                 name="UserDashboard"
@@ -58,7 +46,12 @@ export default () => (
                 options={{ title: "Add Product" }}
             />
             <Stack.Screen
-                name="ProductsList"
+                name="Browse"
+                component={Browse}
+                options={{ title: "Browse Categories" }}
+            />
+            <Stack.Screen
+                name="Products List"
                 component={ProductsList}
                 options={{ title: "ProductsList" }}
             />
@@ -88,7 +81,7 @@ export default () => (
                 options={{ title: "Sign In" }}
             />
             <Stack.Screen
-                name="ProductDetail"
+                name="Product Detail"
                 component={ProductDetail}
                 options={{ title: "ProductDetail" }}
             />
@@ -97,7 +90,6 @@ export default () => (
                 component={SignUpScreen}
                 options={{ title: "Sign Up" }}
             />
->>>>>>> 76e083c87e80876c88bde44c53c26869f3c93c3a
         </Stack.Navigator>
     </NavigationContainer>
 );
