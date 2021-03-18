e8ecb0b42f2886ace97f8effd7f3f2360c540841
Chetan Singh <chetan.singh.0602@gmail.com>
2021-03-18 16:44:29 -0400

    fixed product detail description

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ import SellerLogin from "./components/SellerLogin";
 import SellerSignUp from "./components/SellerSignUp";
 import SellerDashboard from "./components/SellerDashboard";
 import UserDashboard from "./components/UserDashboard";
+import Browse from "./components/Browse";
+import ProductDetail from "./components/ProductDetail";
+import ProductsList from "./components/ProductsList";
+import ContactForBuy from "./components/ContactForBuy";
 
 const Stack = createStackNavigator();
 
@@ -18,7 +22,11 @@ export default () => (
             {/* <Stack.Screen name="SellerLogin" component={SellerLogin} />
             <Stack.Screen name="SellerSignUp" component={SellerSignUp} />
             <Stack.Screen name="SellerDashboard" component={SellerDashboard} /> */}
-            <Stack.Screen name="UserDashboard" component={UserDashboard} />
+            {/* <Stack.Screen name="UserDashboard" component={UserDashboard} /> */}
+            <Stack.Screen name="Browse" component={Browse} />
+            <Stack.Screen name="Products List" component={ProductsList} />
+            <Stack.Screen name="Product Detail" component={ProductDetail} />
+            <Stack.Screen name="Contact For Buy" component={ContactForBuy} />
         </Stack.Navigator>
     </NavigationContainer>
 );
