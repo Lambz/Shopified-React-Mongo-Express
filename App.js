76e083c87e80876c88bde44c53c26869f3c93c3a
chetu97 <chaitanya.sanoriya@gmail.com>
2021-03-18 16:42:39 -0400

    update on user dashboard

--- App.js
+++ App.js
@@ -7,6 +7,15 @@ import SellerLogin from "./components/SellerLogin";
 import SellerSignUp from "./components/SellerSignUp";
 import SellerDashboard from "./components/SellerDashboard";
 import UserDashboard from "./components/UserDashboard";
+import OldOrders from "./components/OldOrders";
+import OrderDetails from "./components/OrderDetails";
+import ChangePassword from "./components/ChangePassword";
+import ContactForBuy from "./components/ContactForBuy";
+import SignInScreen from "./LoginScreen";
+import SignUpScreen from "./SignUpScreen";
+import AddProuct from "./components/AddProduct";
+import ProductDetail from "./components/ProductDetail";
+import ProductsList from "./components/ProductsList";
 
 const Stack = createStackNavigator();
 
@@ -18,7 +27,62 @@ export default () => (
             {/* <Stack.Screen name="SellerLogin" component={SellerLogin} />
             <Stack.Screen name="SellerSignUp" component={SellerSignUp} />
             <Stack.Screen name="SellerDashboard" component={SellerDashboard} /> */}
-            <Stack.Screen name="UserDashboard" component={UserDashboard} />
+
+            <Stack.Screen
+                name="UserDashboard"
+                component={UserDashboard}
+                options={{ title: "Dashboard", headerShown: false }}
+            />
+            <Stack.Screen
+                name="SellerDashboard"
+                component={SellerDashboard}
+                options={{ title: "Seller Dashboard", headerShown: false }}
+            />
+            <Stack.Screen
+                name="AddProuct"
+                component={AddProuct}
+                options={{ title: "Add Product" }}
+            />
+            <Stack.Screen
+                name="ProductsList"
+                component={ProductsList}
+                options={{ title: "ProductsList" }}
+            />
+            <Stack.Screen
+                name="OldOrders"
+                component={OldOrders}
+                options={{ title: "Your Orders" }}
+            />
+            <Stack.Screen
+                name="OrderDetails"
+                component={OrderDetails}
+                options={{ title: "Order Details" }}
+            />
+            <Stack.Screen
+                name="ChangePassword"
+                component={ChangePassword}
+                options={{ title: "Change Password" }}
+            />
+            <Stack.Screen
+                name="ContactForBuy"
+                component={ContactForBuy}
+                options={{ title: "Delivery Details" }}
+            />
+            <Stack.Screen
+                name="SignInScreen"
+                component={SignInScreen}
+                options={{ title: "Sign In" }}
+            />
+            <Stack.Screen
+                name="ProductDetail"
+                component={ProductDetail}
+                options={{ title: "ProductDetail" }}
+            />
+            <Stack.Screen
+                name="SignUpScreen"
+                component={SignUpScreen}
+                options={{ title: "Sign Up" }}
+            />
         </Stack.Navigator>
     </NavigationContainer>
 );
--- components/Browse.js
+++ components/Browse.js
@@ -1,40 +1,67 @@
-import React, {useState,} from 'react';
+import React, { useState } from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
-import CategoryView from './CategoryView';
-import {fetchAllCategoriesAndSubcategories} from '../model/interface';
+import CategoryView from "./CategoryView";
+import { fetchAllCategoriesAndSubcategories } from "../model/interface";
+import CustomHeader from "./CustomHeader";
 
-export default function Browse({navigation}) {
+export default function Browse({ navigation }) {
     const [categories, setCategories] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    
-    if(isLoading) {
+
+    if (isLoading) {
         fetchAllCategoriesAndSubcategories((data) => {
             setCategories(data);
             setIsLoading(false);
-        })
+        });
     }
-    
+
     const subcategoryClicked = (subcategory) => {
         console.log("subcategory fetched", subcategory);
-        navigation.navigate("Products List", subcategory);
-    }
+        route.params.stackMoveCallback("ProductsList", subcategory);
+        // navigation.navigate("Products List", subcategory);
+    };
 
-    return(
+    const loginFunc = () => {
+        route.params.stackMoveCallback("SignInScreen");
+    };
+
+    const logoutFunc = () => {
+        signOut();
+    };
+
+    const searchFunc = (text) => {
+        console.log(text);
+    };
+
+    return (
         <View style={styles.container}>
+            <CustomHeader
+                loginFunc={loginFunc}
+                logoutFunc={logoutFunc}
+                searchFunc={searchFunc}
+            />
             <Text style={styles.headerText}>Browse</Text>
-            <FlatList data={categories} renderItem={(category) => <CategoryView item={category} clickCallback={subcategoryClicked}/>}></FlatList>
+            <FlatList
+                style={{ padding: 5 }}
+                data={categories}
+                renderItem={(category) => (
+                    <CategoryView
+                        item={category}
+                        clickCallback={subcategoryClicked}
+                    />
+                )}
+            ></FlatList>
         </View>
-
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 5
     },
     headerText: {
         fontSize: 40,
-        fontWeight: "bold"
-    }
+        fontWeight: "bold",
+        padding: 5,
+    },
 });
